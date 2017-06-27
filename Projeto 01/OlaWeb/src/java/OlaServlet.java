31b34ec8c8a2c823061425f027419b932da6da12
Elton Rodrigues <eltonrms.leite@hotmail.com>
2017-06-27 16:26:29 -0300

    Primeiro projeto. Apenas um Hello Word!

---
+++ Projeto 01/OlaWeb/src/java/OlaServlet.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Elton
+ */
+@WebServlet(urlPatterns = "/Ola")
+public class OlaServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String nome = request.getParameter("nome");
+        
+        if (nome == null) {
+            nome = "camarada";
+        }
+        response.getWriter().print("<html><h1>Oi " + nome + "!</h1></html>");
+    }
+}
